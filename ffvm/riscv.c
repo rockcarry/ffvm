a50e454a69a9a70c2b650e0bd7b2beb79e3db9c9
chenk <chenk@apical.com.cn>
2020-04-22 11:02:52 +0800

    优化代码

--- ffvm/riscv.c
+++ ffvm/riscv.c
@@ -64,6 +64,10 @@ static uint32_t riscv_memr32(RISCV *riscv, uint32_t addr)
 
 static void riscv_memw32(RISCV *riscv, uint32_t addr, uint32_t data)
 {
+    if (addr == 0x8000) {
+        putchar((int)data);
+        return;
+    }
     if ((addr & 0x3) == 0) {
         *(uint32_t*)(riscv->mem + (addr & (MAX_MEM_SIZE - 1))) = data;
     } else {
@@ -98,7 +102,7 @@ void riscv_run(RISCV *riscv)
     const uint32_t inst_imm20u = instruction & (0xfffff << 12);
     const uint32_t inst_imm21j =((instruction >> 11) & (1 << 20)) | (instruction & (0xff << 12))
                                |((instruction >> 9) & (1 << 11)) | ((instruction >> 20) & (0x3ff << 1));
-    uint32_t bflag = 0, maddr;
+    uint32_t bflag = 0, maddr, temp;
     int64_t  mult64res;
 
     switch (inst_opcode) {
@@ -106,7 +110,7 @@ void riscv_run(RISCV *riscv)
         riscv->x[inst_rd] = inst_imm20u;
         break;
     case 0x17: // u-type auipc
-        riscv->x[inst_rd] = riscv->pc + signed_extend(inst_imm20u, 20);
+        riscv->x[inst_rd] = riscv->pc + (int32_t)inst_imm20u;
         break;
     case 0x6f: // j-type jal
         riscv->x[inst_rd] = riscv->pc + 4;
@@ -116,92 +120,52 @@ void riscv_run(RISCV *riscv)
     case 0x67: // i-type
         switch (inst_funct3) {
         case 0x0: // jalr
-            riscv->x[inst_rd] = riscv->pc + 4;
-            riscv->pc = signed_extend(inst_imm12i, 12) + riscv->x[inst_rs1];
+            temp = riscv->pc + 4;
+            riscv->pc = riscv->x[inst_rs1] + signed_extend(inst_imm12i, 12);
+            riscv->pc&=~(1 << 0);
+            riscv->x[inst_rd] = temp;
             bflag = 1;
             break;
         }
         break;
     case 0x63: // b-type
         switch (inst_funct3) {
-        case 0x0: // beq
-            bflag = riscv->x[inst_rs1] == riscv->x[inst_rs2];
-            break;
-        case 0x1: // bne
-            bflag = riscv->x[inst_rs1] != riscv->x[inst_rs2];
-            break;
-        case 0x4: // blt
-            bflag = (int32_t)riscv->x[inst_rs1] < (int32_t)riscv->x[inst_rs2];
-            break;
-        case 0x5: // bge
-            bflag = (int32_t)riscv->x[inst_rs1] >= (int32_t)riscv->x[inst_rs2];
-            break;
-        case 0x6: // bltu
-            bflag = riscv->x[inst_rs1] < riscv->x[inst_rs2];
-            break;
-        case 0x7: // bgeu
-            bflag = riscv->x[inst_rs1] >= riscv->x[inst_rs2];
-            break;
+        case 0x0: bflag = riscv->x[inst_rs1] == riscv->x[inst_rs2]; break; // beq
+        case 0x1: bflag = riscv->x[inst_rs1] != riscv->x[inst_rs2]; break; // bne
+        case 0x4: bflag = (int32_t)riscv->x[inst_rs1] <  (int32_t)riscv->x[inst_rs2]; break; // blt
+        case 0x5: bflag = (int32_t)riscv->x[inst_rs1] >= (int32_t)riscv->x[inst_rs2]; break; // bge
+        case 0x6: bflag = riscv->x[inst_rs1] <  riscv->x[inst_rs2]; break; // bltu
+        case 0x7: bflag = riscv->x[inst_rs1] >= riscv->x[inst_rs2]; break; // bgeu
         }
         if (bflag) riscv->pc += signed_extend(inst_imm13b, 13);
         break;
     case 0x03: // i-type
         maddr = riscv->x[inst_rs1] + signed_extend(inst_imm12i, 12);
         switch (inst_funct3) {
-        case 0x0: // lb
-            riscv->x[inst_rd] = (int32_t)riscv_memr8(riscv, maddr);
-            break;
-        case 0x1: // lh
-            riscv->x[inst_rd] = (int32_t)riscv_memr16(riscv, maddr);
-            break;
-        case 0x2: // lw
-            riscv->x[inst_rd] = riscv_memr32(riscv, maddr);
-            break;
-        case 0x4: // lbu
-            riscv->x[inst_rd] = riscv_memr8(riscv, maddr);
-            break;
-        case 0x5: // lhu
-            riscv->x[inst_rd] = riscv_memr16(riscv, maddr);
-            break;
+        case 0x0: riscv->x[inst_rd] = (int32_t)riscv_memr8 (riscv, maddr); break; // lb
+        case 0x1: riscv->x[inst_rd] = (int32_t)riscv_memr16(riscv, maddr); break; // lh
+        case 0x2: riscv->x[inst_rd] = riscv_memr32(riscv, maddr); break; // lw
+        case 0x4: riscv->x[inst_rd] = riscv_memr8 (riscv, maddr); break; // lbu
+        case 0x5: riscv->x[inst_rd] = riscv_memr16(riscv, maddr); break; // lhu
         }
         break;
     case 0x23: // s-type
         maddr = riscv->x[inst_rs1] + signed_extend(inst_imm12s, 12);
         switch (inst_funct3) {
-        case 0x0: // sb
-            riscv_memw8(riscv, maddr, (uint8_t)riscv->x[inst_rs2]);
-            break;
-        case 0x1: // sh
-            riscv_memw16(riscv, maddr, (uint16_t)riscv->x[inst_rs2]);
-            break;
-        case 0x2: // sw
-            riscv_memw32(riscv, maddr, riscv->x[inst_rs2]);
-            break;
+        case 0x0: riscv_memw8 (riscv, maddr, (uint8_t )riscv->x[inst_rs2]); break; // sb
+        case 0x1: riscv_memw16(riscv, maddr, (uint16_t)riscv->x[inst_rs2]); break; // sh
+        case 0x2: riscv_memw32(riscv, maddr, riscv->x[inst_rs2]); break; // sw
         }
         break;
     case 0x13: // i-type
         switch (inst_funct3) {
-        case 0x0: // addi
-            riscv->x[inst_rd] = riscv->x[inst_rs1] + signed_extend(inst_imm12i, 12);
-            break;
-        case 0x2: // slti
-            riscv->x[inst_rd] = (int32_t)riscv->x[inst_rs1] < signed_extend(inst_imm12i, 12);
-            break;
-        case 0x3: // sltiu
-            riscv->x[inst_rd] = riscv->x[inst_rs1] < inst_imm12i;
-            break;
-        case 0x4: // xori
-            riscv->x[inst_rd] = riscv->x[inst_rs1] ^ (signed_extend(inst_imm12i, 12));
-            break;
-        case 0x6: // ori
-            riscv->x[inst_rd] = riscv->x[inst_rs1] | (signed_extend(inst_imm12i, 12));
-            break;
-        case 0x7: // andi
-            riscv->x[inst_rd] = riscv->x[inst_rs1] & (signed_extend(inst_imm12i, 12));
-            break;
-        case 0x1: // slli
-            riscv->x[inst_rd] = riscv->x[inst_rs1] << (inst_imm12i & 0x1f);
-            break;
+        case 0x0: riscv->x[inst_rd] = riscv->x[inst_rs1] + signed_extend(inst_imm12i, 12); break; // addi
+        case 0x2: riscv->x[inst_rd] = (int32_t)riscv->x[inst_rs1] < signed_extend(inst_imm12i, 12);  break; // slti
+        case 0x3: riscv->x[inst_rd] = riscv->x[inst_rs1] < (uint32_t)signed_extend(inst_imm12i, 12); break; // sltiu
+        case 0x4: riscv->x[inst_rd] = riscv->x[inst_rs1] ^ (signed_extend(inst_imm12i, 12)); break; // xori
+        case 0x6: riscv->x[inst_rd] = riscv->x[inst_rs1] | (signed_extend(inst_imm12i, 12)); break; // ori
+        case 0x7: riscv->x[inst_rd] = riscv->x[inst_rs1] & (signed_extend(inst_imm12i, 12)); break; // andi
+        case 0x1: riscv->x[inst_rd] = riscv->x[inst_rs1] << (inst_imm12i & 0x1f); break; // slli
         case 0x5: // srli & srai
             if (inst_funct7 & (1 << 5)) { // srai
                 riscv->x[inst_rd] = (int32_t)riscv->x[inst_rs1] >> (inst_imm12i & 0x1f);
@@ -221,18 +185,10 @@ void riscv_run(RISCV *riscv)
                     riscv->x[inst_rd] = riscv->x[inst_rs1] + riscv->x[inst_rs2];
                 }
                 break;
-            case 0x1: // sll
-                riscv->x[inst_rd] = riscv->x[inst_rs1] << (riscv->x[inst_rs2] & 0x1f);
-                break;
-            case 0x2: // slt
-                riscv->x[inst_rd] = (int32_t)riscv->x[inst_rs1] < (int32_t)riscv->x[inst_rs2];
-                break;
-            case 0x3: // sltu
-                riscv->x[inst_rd] = riscv->x[inst_rs1] < riscv->x[inst_rs2];
-                break;
-            case 0x4: // xor
-                riscv->x[inst_rd] = riscv->x[inst_rs1] ^ riscv->x[inst_rs2];
-                break;
+            case 0x1: riscv->x[inst_rd] = riscv->x[inst_rs1] << (riscv->x[inst_rs2] & 0x1f); break; // sll
+            case 0x2: riscv->x[inst_rd] = (int32_t)riscv->x[inst_rs1] < (int32_t)riscv->x[inst_rs2]; break; // slt
+            case 0x3: riscv->x[inst_rd] = riscv->x[inst_rs1] < riscv->x[inst_rs2]; break; // sltu
+            case 0x4: riscv->x[inst_rd] = riscv->x[inst_rs1] ^ riscv->x[inst_rs2]; break; // xor
             case 0x5: // srl & sra
                 if (inst_funct7 & (1 << 5)) { // sra
                     riscv->x[inst_rd] = (int32_t)riscv->x[inst_rs1] >> (riscv->x[inst_rs2] & 0x1f);
@@ -240,28 +196,22 @@ void riscv_run(RISCV *riscv)
                     riscv->x[inst_rd] = riscv->x[inst_rs1] >> (riscv->x[inst_rs2] & 0x1f);
                 }
                 break;
-            case 0x6: // or
-                riscv->x[inst_rd] = riscv->x[inst_rs1] | riscv->x[inst_rs2];
-                break;
-            case 0x7: // and
-                riscv->x[inst_rd] = riscv->x[inst_rs1] & riscv->x[inst_rs2];
-                break;
+            case 0x6: riscv->x[inst_rd] = riscv->x[inst_rs1] | riscv->x[inst_rs2]; break; // or
+            case 0x7: riscv->x[inst_rd] = riscv->x[inst_rs1] & riscv->x[inst_rs2]; break; // and
             }
         } else {
             switch (inst_funct3) {
-            case 0x0: // mul
-                riscv->x[inst_rd] = (uint32_t)((int32_t)riscv->x[inst_rs1] * (int32_t)riscv->x[inst_rs2]);
-                break;
+            case 0x0: riscv->x[inst_rd] = riscv->x[inst_rs1] * riscv->x[inst_rs2]; break; // mul
             case 0x1: // mulh
-                mult64res = (int64_t)riscv->x[inst_rs1] * (int32_t)riscv->x[inst_rs2];
+                mult64res = (int64_t)riscv->x[inst_rs1] * (int64_t)riscv->x[inst_rs2];
                 riscv->x[inst_rd] = (uint32_t)(mult64res >> 32);
                 break;
             case 0x2: // mulhsu
-                mult64res = (int32_t)riscv->x[inst_rs1] * riscv->x[inst_rs2];
+                mult64res = (int64_t)riscv->x[inst_rs1] * (uint64_t)riscv->x[inst_rs2];
                 riscv->x[inst_rd] = (uint32_t)(mult64res >> 32);
                 break;
             case 0x3: // mulhu
-                mult64res = riscv->x[inst_rs1] * riscv->x[inst_rs2];
+                mult64res = (uint64_t)riscv->x[inst_rs1] * (uint64_t)riscv->x[inst_rs2];
                 riscv->x[inst_rd] = (uint32_t)(mult64res >> 32);
                 break;
             case 0x4: // div
