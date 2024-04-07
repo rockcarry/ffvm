41c5a17458affbffce5a80c0a2cfc63e4a9432ce
chenk <chenk@apical.com.cn>
2024-04-07 12:34:16 +0800

    update libavdev.

--- libavdev/include/libavdev/adev.h
+++ libavdev/include/libavdev/adev.h
@@ -11,7 +11,7 @@ void* adev_init(int out_samprate, int out_chnum, int out_frmsize, int out_frmnum
 void  adev_exit(void *ctx);
 int   adev_play(void *ctx, void *buf, int len, int waitms);
 void  adev_set (void *ctx, char *name, void *data); // name: "pause", "resume", "reset", "callback", "cbctx"
-void* adev_get (void *ctx, char *name);
+long  adev_get (void *ctx, char *name, void *data);
 
 int adev_record(void *ctx, int start, int rec_samprate, int rec_chnum, int rec_frmsize, int rec_frmnum);
 
