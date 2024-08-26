2681ed81a15294b07a70d8a3251b69ec624b3f9e
chenk <chenk@apical.com.cn>
2024-08-26 21:12:37 +0800

    修改 ethphy.c 代码，使用 LoadLibrary 动态加载 wpcap.dll

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 CFLAGS="-Wall -Wno-strict-aliasing -Wno-stringop-truncation -Ofast -g -I$PWD/libavdev/include -I$PWD/libpcap/include"
-LDFLAGS="-L$PWD/libavdev/lib -L$PWD/libpcap/lib -lavdev -lpcap -lgdi32 -lwinmm"
+LDFLAGS="-L$PWD/libavdev/lib -lavdev -lgdi32 -lwinmm"
 
 gcc --static $CFLAGS utils.c ethphy.c ffvm.c $LDFLAGS -o ffvm
 strip ffvm.exe
--- ethphy.c
+++ ethphy.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
@@ -9,6 +10,17 @@ typedef struct {
     #define FLAG_EXIT (1 << 0)
     uint32_t  flags;
     pthread_t thread;
+
+    HMODULE   hDll;
+    pcap_t* (*pcap_open_live)(const char*, int, int, int, char*);
+    void    (*pcap_close)(pcap_t*);
+    int     (*pcap_findalldevs)(pcap_if_t**, char*);
+    void    (*pcap_freealldevs)(pcap_if_t*);
+    int     (*pcap_loop)(pcap_t*, int, pcap_handler, u_char*);
+    void    (*pcap_breakloop)(pcap_t*);
+    int     (*pcap_sendpacket)(pcap_t*, const u_char*, int);
+    char*   (*pcap_geterr)(pcap_t*);
+
     PFN_ETHPHY_CALLBACK callback;
     void               *cbctx;
 } PHYDEV;
@@ -23,7 +35,7 @@ static void* ethphy_work_proc(void *arg)
 {
     PHYDEV *phy = arg;
     while (!(phy->flags & FLAG_EXIT)) {
-        pcap_loop(phy->pcap, 0, packet_handler, (u_char*)phy);
+        phy->pcap_loop(phy->pcap, 0, packet_handler, (u_char*)phy);
     }
     return NULL;
 }
@@ -35,9 +47,32 @@ void* ethphy_open(int dev, PFN_ETHPHY_CALLBACK callback, void *cbctx)
     char errbuf[PCAP_ERRBUF_SIZE];
     int  i = 0, n;
 
-    if (pcap_findalldevs(&alldevs, errbuf) == -1) {
+    PHYDEV *phy = calloc(1, sizeof(PHYDEV));
+    if (!phy) return NULL;
+
+    phy->hDll = LoadLibrary(TEXT("wpcap.dll"));
+    if (!phy->hDll) {
+        fprintf(stderr, "Error in load wpcap.dll library !\n");
+        goto failed;
+    }
+
+    phy->pcap_open_live   = (void*)GetProcAddress(phy->hDll, "pcap_open_live");
+    phy->pcap_findalldevs = (void*)GetProcAddress(phy->hDll, "pcap_findalldevs");
+    phy->pcap_freealldevs = (void*)GetProcAddress(phy->hDll, "pcap_freealldevs");
+    phy->pcap_loop        = (void*)GetProcAddress(phy->hDll, "pcap_loop");
+    phy->pcap_breakloop   = (void*)GetProcAddress(phy->hDll, "pcap_breakloop");
+    phy->pcap_sendpacket  = (void*)GetProcAddress(phy->hDll, "pcap_sendpacket");
+    phy->pcap_geterr      = (void*)GetProcAddress(phy->hDll, "pcap_geterr");
+    if (  !phy->pcap_open_live || !phy->pcap_findalldevs || !phy->pcap_freealldevs || !phy->pcap_loop
+       || !phy->pcap_breakloop || !phy->pcap_sendpacket || !phy->pcap_geterr)
+    {
+        fprintf(stderr, "Error in GetProcAddress !\n");
+        goto failed;
+    }
+
+    if (phy->pcap_findalldevs(&alldevs, errbuf) == -1) {
         fprintf(stderr, "Error in pcap_findalldevs: %s\n", errbuf);
-        return NULL;
+        goto failed;
     }
 
     for (d = alldevs; d; d = d->next) {
@@ -51,7 +86,7 @@ void* ethphy_open(int dev, PFN_ETHPHY_CALLBACK callback, void *cbctx)
 
     if (i == 0) {
         printf("\nNo interfaces found ! Make sure WinPcap is installed.\n");
-        return NULL;
+        goto failed;
     }
 
     if (dev < 1 || dev > i) {
@@ -63,15 +98,15 @@ void* ethphy_open(int dev, PFN_ETHPHY_CALLBACK callback, void *cbctx)
 
     if (n < 1 || n > i) {
         printf("\nInterface number out of range.\n");
-        pcap_freealldevs(alldevs);
-        return NULL;
+        phy->pcap_freealldevs(alldevs);
+        goto failed;
     }
 
     /* Jump to the selected adapter */
     for (d = alldevs, i = 0; i < n - 1; d = d->next, i++);
 
     /* Open the adapter */
-    if ((pcap = pcap_open_live(d->name,
+    if ((pcap = phy->pcap_open_live(d->name,
         65536, // portion of the packet to capture.
         1,     // promiscuous mode (nonzero means promiscuous)
         1,     // read timeout, 0 blocked, -1 no timeout
@@ -79,21 +114,22 @@ void* ethphy_open(int dev, PFN_ETHPHY_CALLBACK callback, void *cbctx)
         )) == NULL)
     {
         fprintf(stderr, "\nUnable to open the adapter. %s is not supported by WinPcap\n", d->name);
-        pcap_freealldevs(alldevs);
-        return NULL;
+        phy->pcap_freealldevs(alldevs);
+        goto failed;
     }
 
     printf("\nlistening on %s...\n", d->description);
-    pcap_freealldevs(alldevs);
-
-    PHYDEV *phy = calloc(1, sizeof(PHYDEV));
-    if (!phy) { pcap_close(pcap); return NULL; }
+    phy->pcap_freealldevs(alldevs);
 
     phy->pcap     = pcap;
     phy->callback = callback;
     phy->cbctx    = cbctx;
     pthread_create(&phy->thread, 0, ethphy_work_proc, phy);
     return phy;
+
+failed:
+    ethphy_close(phy);
+    return NULL;
 }
 
 void ethphy_close(void *ctx)
@@ -101,9 +137,10 @@ void ethphy_close(void *ctx)
     PHYDEV *phy = ctx;
     if (phy) {
         phy->flags |= FLAG_EXIT;
-        pcap_breakloop(phy->pcap);
-        pthread_join(phy->thread, NULL);
-        pcap_close(phy->pcap);
+        if (phy->pcap  ) phy->pcap_breakloop(phy->pcap);
+        if (phy->thread) pthread_join(phy->thread, NULL);
+        if (phy->pcap  ) phy->pcap_close(phy->pcap);
+        if (phy->hDll  ) FreeLibrary(phy->hDll);
         free(phy);
     }
 }
@@ -112,8 +149,8 @@ int ethphy_send(void *ctx, char *buf, int len)
 {
     if (!ctx) return -1;
     PHYDEV *phy = ctx;
-    if (pcap_sendpacket(phy->pcap, (const u_char*)buf, len) != 0) {
-        fprintf(stderr, "\nError sending the packet: %s\n", pcap_geterr(phy->pcap));
+    if (phy->pcap_sendpacket(phy->pcap, (const u_char*)buf, len) != 0) {
+        fprintf(stderr, "\nError sending the packet: %s\n", phy->pcap_geterr(phy->pcap));
         return -1;
     }
     return 0;
